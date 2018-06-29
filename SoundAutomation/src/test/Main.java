e71a3dc93722831f7abfc3d4e804900c0c31ad93
thaleradm <thaleradm@ilzwinihprod015.sgi2.root.magnapowertrain.com>
2018-06-29 12:30:51 +0200

    ...

--- SoundAutomation/src/test/Main.java
+++ SoundAutomation/src/test/Main.java
@@ -1,4 +1,4 @@
-package application;
+package test;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
--- SoundAutomation/src/test/SoundPlayer.java
+++ SoundAutomation/src/test/SoundPlayer.java
@@ -1,4 +1,4 @@
-package application;
+package test;
 
 import java.io.File;
 import java.io.FileInputStream;
